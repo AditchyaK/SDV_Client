35b0c590fe18908cca720f7d9d1acf00faea20a7
Aditya Kondapuram <kondapuram.aditya@gmail.com>
2019-04-29 17:54:24 -0300

    added to README.md and commented clienttest.py properly

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ﻿##South Shore Robotics Underwater ROV
 This is the code for a microcontroller on an underwater ROV by a surface laptop running linux and sending input from an Xbox controller
+Dependencies:
+adafruit_motorkit -> sudo pip3 install adafruit-circuitpython-motorkit
+For setting up the motor hat from the beginning:
+https://learn.adafruit.com/adafruit-dc-and-stepper-motor-hat-for-raspberry-pi/overview
--- clienttest.py
+++ clienttest.py
@@ -1,8 +1,6 @@
 import sys, socket, time
 from adafruit_motorkit import MotorKit
 
-port = 5558
-host = '169.254.227.12'
 prevVal = 0
 keys = ("AB", "BB", "XB", "YB", "LH", "RH", "DU", "DD", "DL", "DR", "LB", "RB", "LX", "LY", "RX", "RY", "LT", "RT", "END")
 
@@ -33,6 +31,8 @@ RT = move vertical motors
 
 """
 
+#this function splits the incoming string which contains xbox controller input into
+#an array of integers (for the buttons) and floating point numbers (for others)
 def splitData(data):
     datatru = []
     datanu = data.split(keys[0])
@@ -47,6 +47,7 @@ def splitData(data):
             datatru[i] = float(datatru[i])
     return datatru
 
+#WIP: this function is responsible for toggling a button 
 def buttonToggle(buttonVal, prevVal):
     if buttonVal:
         if (prevVal == 0):
@@ -57,7 +58,9 @@ def buttonToggle(buttonVal, prevVal):
     else:
         return prevVal, buttonVal
 
+#this class contains all the control methods such as for the claw, motors and lights
 class controlsClass:
+    #this claw function is responsible for controlling the claw with two buttons
     def claw(self, button1, button2):
         if button1:
             kit.motor1.throttle = 1.0 #change sign according to polarity
@@ -73,27 +76,38 @@ sent by the server and converting it to an array of values to use as controls
 #initializing controls class
 controls = controlsClass()
 
+#initalizing the MotorKit class
 try:
     kit = MotorKit()
     print("Motor Class has been initialized")
 except:
     print("Motor Class could not be initialized")
 
+#creating a scoket and then setting it to resuse the port when shut down
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 print("Socket has been created")
 
+#initalizing host and port variables
+port = 5558
+host = '169.254.227.12'
+
+#client socket connects to the server with static ip address
 try: 
     s.connect((host, port))
     print("Socket successfully binded to ", host, " on port ", port)
 except socket.error as msg:
     print("Socket failed to connect to host /n", msg)
 
+#main loop for the program -> checks for data being sent by server in packets and
+#then sending respective input to the cntroller class which operates motors
 while True:
     data = s.recv(1024)
     data = data.decode('utf-8')
+    #this disconnects the client (this device) from the server
     if (data == "KILL"):
         break
+    #this is to set motors to stop when the controller is disconnected
     elif (data == "HOLD"):
         #stop all motors
         print("HOLD")
@@ -106,6 +120,8 @@ while True:
         print(str(data[i]), end=" ")
     """
 
+#cleaning up everything at the end
+conn.close()
 s.close()
 print("\nYou have been disconnected from the server")
 sys.exit()
